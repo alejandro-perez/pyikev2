aca5e9c60fc22a4f9d0f6eec100b8be9c9d199a8
Alejandro Perez <alex@um.es>
2017-03-10 21:05:40 +0000

    Implemented encryption module

--- encr.py
+++ encr.py
@@ -6,6 +6,11 @@
 
 from helpers import SafeIntEnum
 
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+
 class Cipher:
     class Id(SafeIntEnum):
         ENCR_DES_IV64 = 1
@@ -20,3 +25,32 @@ class Cipher:
         ENCR_NULL = 11
         ENCR_AES_CBC = 12
         ENCR_AES_CTR = 13
+
+    _algorithm_dict = {
+        Id.ENCR_AES_CBC: algorithms.AES,
+        Id.ENCR_3DES: algorithms.TripleDES,
+    }
+
+    def __init__(self, transform_id, negotiated_keylen):
+        self._algorithm = self._algorithm_dict[transform_id]
+        self._backend = default_backend()
+        self.negotiated_keylen = negotiated_keylen
+
+    @property
+    def block_size(self):
+        return self._algorithm.block_size // 8
+
+    @property
+    def key_size(self):
+        return (self.negotiated_keylen or self._algorithm.key_sizes[0]) // 8
+
+    def encrypt(self, key, data):
+        iv = os.urandom(self.block_size)
+        cipher = Cipher(self._algorithm(key), modes.CBC(iv), backend=self._backend)
+        encryptor = cipher.encryptor()
+        return encryptor.update(data) + encryptor.finalize()
+
+    def decrypt(self, key, data):
+        cipher = Cipher(self._algorithm(key), modes.CBC(iv), backend=self._backend)
+        encryptor = cipher.decryptor()
+        return decryptor.update(data) + decryptor.finalize()
