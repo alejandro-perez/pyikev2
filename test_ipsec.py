c9a45b9f762ba3b55b5465975bd48ea3168c7815
Alejandro Perez <alejandro.perez.mendez@gmail.com>
2017-03-15 14:05:32 +0100

    Add help fnc and make 2 direction policies

--- test_ipsec.py
+++ test_ipsec.py
@@ -19,10 +19,13 @@ class TestIpsec(unittest.TestCase):
             Policy.Mode.TRANSPORT)
         ipsec.create_policy(policy)
         text_pol = subprocess.check_output(['ip', 'xfrm', 'policy'])
-        self.assertEqual(text_pol,
-                         b'src 192.168.1.0/24 dst 10.0.0.0/8 proto tcp dport '
-                         b'80 \n\tdir out priority 0 \n\ttmpl src 0.0.0.0 dst '
-                         b'0.0.0.0\n\t\tproto ah reqid 0 mode transport\n')
+        self.assertEqual(
+            text_pol,
+            b'src 10.0.0.0/8 dst 192.168.1.0/24 proto tcp sport 80 \n\tdir in '
+            b'priority 0 \n\ttmpl src 0.0.0.0 dst 0.0.0.0\n\t\tproto ah reqid '
+            b'0 mode transport\nsrc 192.168.1.0/24 dst 10.0.0.0/8 proto tcp dp'
+            b'ort 80 \n\tdir out priority 0 \n\ttmpl src 0.0.0.0 dst 0.0.0.0\n'
+            b'\t\tproto ah reqid 0 mode transport\n')
 
     def test_create_tunnel_policy(self):
         policy = Policy('192.168.1.0/24', 0, '10.0.0.0/8', 80,
@@ -30,11 +33,14 @@ class TestIpsec(unittest.TestCase):
             Policy.Mode.TUNNEL, '155.54.1.1', '155.54.1.2')
         ipsec.create_policy(policy)
         text_pol = subprocess.check_output(['ip', 'xfrm', 'policy'])
-        self.assertEqual(text_pol,
-                         b'src 192.168.1.0/24 dst 10.0.0.0/8 proto tcp dport '
-                         b'80 \n\tdir out priority 0 \n\ttmpl src 155.54.1.1 '
-                         b'dst 155.54.1.2\n\t\tproto ah reqid 0 mode tunnel\n')
-
+        print(text_pol)
+        self.assertEqual(
+            text_pol,
+            b'src 10.0.0.0/8 dst 192.168.1.0/24 proto tcp sport 80 \n\tdir in '
+            b'priority 0 \n\ttmpl src 155.54.1.2 dst 155.54.1.1\n\t\tproto ah '
+            b'reqid 0 mode tunnel\nsrc 192.168.1.0/24 dst 10.0.0.0/8 proto tcp'
+            b' dport 80 \n\tdir out priority 0 \n\ttmpl src 155.54.1.1 dst 155'
+            b'.54.1.2\n\t\tproto ah reqid 0 mode tunnel\n')
 
     def tearDown(self):
         subprocess.call(['ip', 'xfrm', 'policy', 'flush'])
