47f56f12045dbee06d3fa75a5e7e3b193036f6ac
Alejandro Perez <alejandro.perez.mendez@gmail.com>
2017-03-15 21:54:46 +0100

    Included command line arguments for src and dst selectors

--- pyikev2.py
+++ pyikev2.py
@@ -10,9 +10,12 @@ from message import Message, PayloadID, TrafficSelector, Proposal
 from protocol import IkeSaController
 from ipaddress import ip_address
 from ipsec import Policy
+import ipsec
+import json
 
 # parses the arguments
-parser = argparse.ArgumentParser(description='Opensource IKEv2 daemon written in Python.')
+parser = argparse.ArgumentParser(
+    description='Opensource IKEv2 daemon written in Python.')
 parser.add_argument('--verbose', '-v', action='store_true',
     help='Enable (much) more verbosity. WARNING: This will make your key '
     'material to be shown in the log output!')
@@ -25,9 +28,13 @@ parser.add_argument('--use-ip-id', '-ip', action='store_true',
     help='Whether to use the current IP address as our identity.'
     ' It has precedence over --email.')
 parser.add_argument('--pre-shared-key', '-psk', required=True, metavar='KEY',
-    help='The PSK to be used for authentication.'),
+    help='The PSK to be used for authentication.')
 parser.add_argument('--indent-json', '-i', type=int, default=None, metavar='N',
-    help='Indent JSON log output with the provided number of spaces.'),
+    help='Indent JSON log output with the provided number of spaces.')
+parser.add_argument('--src-selector', '-src', required=True,
+    metavar='IP/MASK:PORT', help='Source selector of protected traffic')
+parser.add_argument('--dst-selector', '-dst', required=True,
+    metavar='IP/MASK:PORT', help='Destination selector of protected traffic')
 args = parser.parse_args()
 
 # set logger
@@ -49,10 +56,15 @@ if args.use_ip_id:
 else:
     my_id = PayloadID(PayloadID.Type.ID_RFC822_ADDR, args.email_id.encode())
 
-policy = Policy('10.0.5.141/32', 23, '10.0.5.0/24', 0,
-    TrafficSelector.IpProtocol.TCP, Proposal.Protocol.ESP,
+# create the policy
+policy = Policy(
+    args.src_selector.split(':')[0], args.src_selector.split(':')[1],
+    args.dst_selector.split(':')[0], args.dst_selector.split(':')[1],
+    TrafficSelector.IpProtocol.ANY, Proposal.Protocol.ESP,
     Policy.Mode.TRANSPORT)
 
+logging.debug('Creating IPsec policy: {}'.format(
+    json.dumps(policy.to_dict(), indent=logging.indent_json)))
 # create IkeSaController
 ike_sa_controller = IkeSaController(
     psk=args.pre_shared_key.encode(),
