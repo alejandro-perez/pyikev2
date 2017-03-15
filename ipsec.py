92ca06b3b7a25f00f540549d285263f511de8c19
Alejandro Perez <alejandro.perez.mendez@gmail.com>
2017-03-15 13:43:48 +0100

    Defined create_policy()

--- ipsec.py
+++ ipsec.py
@@ -6,4 +6,58 @@
 
 # TODO: Implement this with the NETLINK interface rather than using ip xfrm command
 import subprocess
+from protocol import Policy
+from message import TrafficSelector, Proposal
 
+class IpsecError(Exception):
+    pass
+
+def create_policy(policy):
+    """ Creates all the directions of a Policy object
+    """
+    _ip_proto_names = {
+        TrafficSelector.IpProtocol.TCP: 'tcp',
+        TrafficSelector.IpProtocol.UDP: 'udp',
+    }
+
+    _ipsec_proto_names = {
+        Proposal.Protocol.ESP: 'esp',
+        Proposal.Protocol.AH: 'ah',
+    }
+    _mode_names = {
+        Policy.Mode.TUNNEL: 'tunnel',
+        Policy.Mode.TRANSPORT: 'transport',
+    }
+
+    command = ['ip', 'xfrm', 'policy', 'add',
+        'src', str(policy.src_selector),
+        'dst', str(policy.dst_selector),
+        'proto', _ip_proto_names[policy.ip_protocol],
+        'sport', str(policy.src_port),
+        'dport', str(policy.dst_port),
+        'dir', 'out',
+        'action', 'allow',
+        'tmpl'
+    ]
+
+    # if tunnel model, add src and dst
+    if policy.mode == Policy.Mode.TUNNEL:
+        command += [
+            'src', str(policy.tunnel_src),
+            'dst', str(policy.tunnel_dst),
+        ]
+    command += [
+        'proto', _ipsec_proto_names[policy.ipsec_protocol],
+        'mode', _mode_names[policy.mode],
+    ]
+
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    try:
+        outs, errs = proc.communicate(timeout=15)
+    except:
+        proc.kill()
+        raise IpsecError('Timeout sending ip xfrm command')
+
+    if proc.poll() != 0:
+        raise IpsecError('Could not create IPsec policy: {}'.format(errs))
--- test_ipsec.py
+++ test_ipsec.py
@@ -3,3 +3,41 @@
 
 """ This module defines test for the IPsec module
 """
+import unittest
+import ipsec
+from protocol import Policy
+from message import TrafficSelector, Proposal
+import subprocess
+
+class TestIpsec(unittest.TestCase):
+    def setUp(self):
+        subprocess.call(['ip', 'xfrm', 'policy', 'flush'])
+
+    def test_create_transport_policy(self):
+        policy = Policy('192.168.1.0/24', 0, '10.0.0.0/8', 80,
+            TrafficSelector.IpProtocol.TCP, Proposal.Protocol.AH,
+            Policy.Mode.TRANSPORT)
+        ipsec.create_policy(policy)
+        text_pol = subprocess.check_output(['ip', 'xfrm', 'policy'])
+        self.assertEqual(text_pol,
+                         b'src 192.168.1.0/24 dst 10.0.0.0/8 proto tcp dport '
+                         b'80 \n\tdir out priority 0 \n\ttmpl src 0.0.0.0 dst '
+                         b'0.0.0.0\n\t\tproto ah reqid 0 mode transport\n')
+
+    def test_create_tunnel_policy(self):
+        policy = Policy('192.168.1.0/24', 0, '10.0.0.0/8', 80,
+            TrafficSelector.IpProtocol.TCP, Proposal.Protocol.AH,
+            Policy.Mode.TUNNEL, '155.54.1.1', '155.54.1.2')
+        ipsec.create_policy(policy)
+        text_pol = subprocess.check_output(['ip', 'xfrm', 'policy'])
+        self.assertEqual(text_pol,
+                         b'src 192.168.1.0/24 dst 10.0.0.0/8 proto tcp dport '
+                         b'80 \n\tdir out priority 0 \n\ttmpl src 155.54.1.1 '
+                         b'dst 155.54.1.2\n\t\tproto ah reqid 0 mode tunnel\n')
+
+
+    def tearDown(self):
+        subprocess.call(['ip', 'xfrm', 'policy', 'flush'])
+
+if __name__ == '__main__':
+    unittest.main()
