f4bba06518278a9ab80aad419c3c91581e28c715
Alejandro Perez <alex@um.es>
2017-03-16 13:23:16 +0100

    removed --listen in favour of --interface. This makes more clear that a specific valid IP must be selected

--- pyikev2.py
+++ pyikev2.py
@@ -12,6 +12,8 @@ from ipaddress import ip_address
 from ipsec import Policy
 import ipsec
 import json
+import netifaces
+import sys
 
 # parses the arguments
 parser = argparse.ArgumentParser(
@@ -19,8 +21,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--verbose', '-v', action='store_true',
     help='Enable (much) more verbosity. WARNING: This will make your key '
     'material to be shown in the log output!')
-parser.add_argument('--listen', '-l', default='', metavar='IP',
-    help='IP address where the daemon will listen from. Defaults to 0.0.0.0.')
+parser.add_argument('--interface', '-i', required=True, metavar='IFACE',
+    help='Interface where the daemon will listen from.')
 parser.add_argument('--email-id', '-e', default='pyikev2@github.com',
     help='An email address to be used as our identity. '
     'Defaults to "pyikev2@github.com"', metavar='EMAIL')
@@ -37,6 +39,16 @@ parser.add_argument('--dst-selector', '-dst', required=True,
     metavar='IP/MASK:PORT', help='Destination selector of protected traffic')
 args = parser.parse_args()
 
+try:
+    addrs = netifaces.ifaddresses(args.interface)
+    ip = addrs[netifaces.AF_INET][0]['addr']
+except ValueError as ex:
+    print(ex)
+    sys.exit(1)
+except KeyError:
+    print('Interface do not have a valid IPv4 number')
+    sys.exit(1)
+
 # set logger
 logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO,
     format='[%(asctime)s.%(msecs)03d] [%(levelname)-6s] %(message)s',
@@ -45,7 +57,7 @@ logging.indent_spaces = args.indent_spaces
 
 # create socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((args.listen, 500))
+sock.bind((ip, 500))
 logging.info('Listening from {}'.format(sock.getsockname()))
 
 # create the template identity payload
