e8894633f5f7ccd5ae2c487198d47c7af7b93dd8
Alejandro Perez <alex@um.es>
2017-03-17 13:23:54 +0000

    Added the IkeConfiguration class

--- configuration.py
+++ configuration.py
@@ -3,25 +3,51 @@
 
 """ This module defines the classes for the protocol handling.
 """
-from ipaddress import ip_network
+from ipaddress import ip_network, ip_address
 
 class ConfigurationError(Exception):
     pass
+
+class ConfigurationNotFound(ConfigurationError):
+    pass
+
+
+class IkeConfiguration(object):
+    """ Represents the configuration applicable to an individual IKE SA
+    """
+    def __init__(self, key, conf_dict):
+        """ Creates a new IkeConfiguration object from a textual dict 
+            (e.g. comming fron JSON or YAML)
+        """
+        try:
+            self.psk = conf_dict['psk']
+            self.id = conf_dict['email']
+        except KeyError as ex:
+            raise ConfigurationError(
+                'Required parameter "{}" missing for IKE configuration "{}"'.format(
+                    ex.args[0], key))
 
 class Configuration(object):
     """ Represents the daemon configuration
         Basically, a collection of IkeConfigurations
     """ 
     def __init__(self, conf_dict):
-        """ Loads configuration from a dict object
+        """ Creates a new Configuration object from a textual dict 
+            (e.g. comming fron JSON or YAML)
         """
         self._ike_configurations = {}
         for ip_range, ike_conf_dict in conf_dict.items():
             try:
                 ip_range = ip_network(ip_range)
-                self._ike_configurations[ip_range] = ike_conf_dict
+                self._ike_configurations[ip_range] = IkeConfiguration(
+                    ip_range, ike_conf_dict)
             except ValueError as ex:
                 raise ConfigurationError(str(ex))
 
     def get_ike_configuration(self, addr):
-        return self._ike_configurations[str(addr)]
+        addr = ip_address(addr)
+        try:
+            found = next(key for key in self._ike_configurations if addr in key)
+        except StopIteration:
+            raise ConfigurationNotFound
+        return self._ike_configurations[found]
--- test_configuration.py
+++ test_configuration.py
@@ -6,8 +6,14 @@
 __author__ = 'Alejandro Perez <alex@um.es>'
 
 import unittest
-from configuration import Configuration, ConfigurationError
+from ipaddress import ip_network, ip_address
+from configuration import (Configuration, ConfigurationError, 
+    ConfigurationNotFound, IkeConfiguration)
 
+class TestIkeConfiguration(unittest.TestCase):
+    def test_empty(self):
+        with self.assertRaises(ConfigurationError):
+            conf = IkeConfiguration(ip_network('192.168.1.0/24'), {})
 
 class TestConfiguration(unittest.TestCase):
     def test_empty(self):
@@ -15,14 +21,42 @@ class TestConfiguration(unittest.TestCase):
 
     def test_no_range(self):
         with self.assertRaises(ConfigurationError):
-            conf = Configuration({'a': 123})
+            conf = Configuration({'a': {}})
 
     def test_ip(self):
-        conf = Configuration({'192.168.1.1': 123})
+        conf = Configuration({
+            '192.168.1.1': {
+                'psk': 'aa',
+                'email': 'alex@um.es'
+            }
+        })
 
     def test_valid(self):
-        conf = Configuration({'192.168.1.1/32': 123})
-        print(conf._ike_configurations)
+        conf = Configuration({
+            '192.168.1.1/32': {
+                'psk': 'aa',
+                'email': 'alex@um.es'
+            }
+        })
+
+    def test_found(self):
+        conf = Configuration({
+            '192.168.1.0/24': {
+                'psk': 'aa',
+                'email': 'alex@um.es'
+            }
+        })
+        ike_conf = conf.get_ike_configuration('192.168.1.5')
+
+    def test_not_found(self):
+        conf = Configuration({
+            '192.168.1.0/24': {
+                'psk': 'aa',
+                'email': 'alex@um.es'
+            }
+        })
+        with self.assertRaises(ConfigurationNotFound):
+            ike_conf = conf.get_ike_configuration('192.168.2.5')
 
 if __name__ == '__main__':
     unittest.main()
