9aebb1f38bbf7edfdb104c8094af951ad784e271
Alejandro Perez <alex@um.es>
2017-03-20 12:31:55 +0000

    Narrowing now uses IPsec configuration

--- protocol.py
+++ protocol.py
@@ -88,7 +88,7 @@ class IkeSa(object):
     """ This class controls the state machine of a IKE SA
         It is triggered with received Messages and/or IPsec events
     """
-    def __init__(self, is_initiator, policies, configuration):
+    def __init__(self, is_initiator, configuration):
         self.state = IkeSa.State.INITIAL
         self.my_spi = SystemRandom().randint(0, 0xFFFFFFFFFFFFFFFF)
         self.peer_spi = 0
@@ -99,7 +99,6 @@ class IkeSa(object):
         self.chosen_proposal = None
         self.my_crypto = None
         self.peer_crypto = None
-        self.policies = policies
         self.configuration = configuration
 
     @property
@@ -219,23 +218,43 @@ class IkeSa(object):
         )
         return self._select_best_sa_proposal(my_proposal, peer_payload_sa)
 
-    def _select_best_traffic_selector(self, payload_tsi, payload_tsr):
-        """ Selects best matching traffic selectors.
-            Only executed on the responder side, thus we need to use policy's
-            reversed TSs.
+    def _generate_traffic_selectors(self, ipsec_conf):
+        """ Generates traffic selectors based on an ipsec configuration
+        """
+        conf_tsi = TrafficSelector(
+            TrafficSelector.Type.TS_IPV4_ADDR_RANGE, 
+            ipsec_conf['ip_proto'], 
+            ipsec_conf['my_port'],
+            65535 if ipsec_conf['my_port'] == 0 else ipsec_conf['my_port'],
+            ipsec_conf['my_subnet'][0],
+            ipsec_conf['my_subnet'][-1])
+        conf_tsr = TrafficSelector(
+            TrafficSelector.Type.TS_IPV4_ADDR_RANGE, 
+            ipsec_conf['ip_proto'], 
+            ipsec_conf['peer_port'],
+            65535 if ipsec_conf['peer_port'] == 0 else ipsec_conf['peer_port'],
+            ipsec_conf['peer_subnet'][0],
+            ipsec_conf['peer_subnet'][-1])
+        return (conf_tsi, conf_tsr)
+
+    def _get_ipsec_configuration(self, payload_tsi, payload_tsr):
+        """ Find matching IPsec configuration.
             It iterates over the received TS in reversed order and returns the
-            first pair that intersecs with any of our policies
+            first configuration that is bigger than our selectors, and the 
+            narrowed selectors as well
         """
         for tsi in reversed(payload_tsi.traffic_selectors):
             for tsr in reversed(payload_tsr.traffic_selectors):
-                for policy in self.policies:
-                    policy_tsi = policy.get_tsi()
-                    policy_tsr = policy.get_tsr()
-                    narrowed_tsi = tsi.intersection(policy_tsr)
-                    narrowed_tsr = tsr.intersection(policy_tsi)
+                for ipsec_conf in self.configuration['protect']:
+                    (conf_tsi, 
+                     conf_tsr) = self._generate_traffic_selectors(ipsec_conf)
+                    narrowed_tsi = tsi.intersection(conf_tsi)
+                    narrowed_tsr = tsr.intersection(conf_tsr)
                     if narrowed_tsi and narrowed_tsr:
-                        return (narrowed_tsi, narrowed_tsr)
-        raise InvalidSelectors('TS could not be matched with any Policy')
+                        return ipsec_conf, narrowed_tsi, narrowed_tsr
+
+        raise InvalidSelectors(
+            'TS could not be matched with any IPsec configuration')
 
     def log_message(self, message, addr, data, send=True):
         logging.info('IKE_SA: {}. {} {} {} ({} bytes) {} {}'.format(
@@ -420,13 +439,14 @@ class IkeSa(object):
         if auth_data != request_payload_auth.auth_data:
             raise AuthenticationFailed('Invalid AUTH data received')
 
+        # find matching IPsec configuration and TS 
+        # (reverse order as we are responders)
+        (ipsec_conf, chosen_tsr, chosen_tsi) = self._get_ipsec_configuration(
+            request_payload_tsr, request_payload_tsi)
+
         # generate the response payload SA with the chosen proposal
         chosen_child_proposal = self._select_best_child_sa_proposal(
             request_payload_sa)
-
-        # find matching TS
-        chosen_tsi, chosen_tsr = self._select_best_traffic_selector(
-            request_payload_tsi, request_payload_tsr)
 
         # TODO: Take this SPI from an actual acquire to avoid (unlikely) collisions
         chosen_child_proposal.spi = os.urandom(4)
@@ -507,11 +527,6 @@ class IkeSa(object):
 class IkeSaController:
     def __init__(self, configuration):
         self.ike_sas = {}
-        self.policies = [
-            Policy('10.0.5.17/32', 23, '10.0.5.0/24', 0,
-                TrafficSelector.IpProtocol.TCP, Proposal.Protocol.ESP,
-                Policy.Mode.TRANSPORT),
-        ]
         self.configuration = configuration
 
     def dispatch_message(self, data, addr):
@@ -523,8 +538,7 @@ class IkeSaController:
             # look for matching configuration
             ike_configuration = self.configuration.get_ike_configuration(addr[0])
 
-            ike_sa = IkeSa(is_initiator=False,
-                           policies=self.policies, configuration=ike_configuration)
+            ike_sa = IkeSa(is_initiator=False, configuration=ike_configuration)
             self.ike_sas[ike_sa.my_spi] = ike_sa
             logging.info('Starting the creation of IKE SA with SPI={}. Count={}'.format(
                 hexstring(pack('>Q', ike_sa.my_spi)), len(self.ike_sas)))
